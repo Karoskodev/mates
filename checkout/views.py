37d20ab3e41c9c0818dc672838dadb72408d2619
Karosko dev <t.karavasilev@gmail.com>
2024-02-19 21:03:37 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OY5MYIPooaFnGz0eF3smnixjmqbOeStdoVA0ug2zLbVkbWKQBoTQmADRTNuxGLCXP4OuFYl0lB3zbzrp5PzdVDH00GTUCjDEZ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
