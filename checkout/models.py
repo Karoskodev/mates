8a82d204d5fffceeaa73fb160a07762f13b2bbb2
Karosko dev <t.karavasilev@gmail.com>
2024-02-20 20:34:48 +0000

    Added checkout sussess logic

--- checkout/models.py
+++ checkout/models.py
@@ -33,11 +33,11 @@ class Order(models.Model):
         Update grand totaleach time a line item is added,
         accountig for delivery cost.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
-            salf.delivery_cost = 0
+            self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
@@ -47,7 +47,7 @@ class Order(models.Model):
         """ Override original save method to set the order number if it hasnt been set already """
 
         if not self.order_number:
-            self.order.number = self._generate_order_number()
+            self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -64,7 +64,7 @@ class OrderLineItem(models.Model):
     def save(self, *args, **kwargs):
         """ Override original save method to set the lineitem total and update the order total """
 
-        self.lineitem_total = self.product.price * self.quanity
+        self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
