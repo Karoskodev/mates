9826c8dbccf6fdc6a0a616f8f6facf5f1910f59e
Karosko dev <t.karavasilev@gmail.com>
2024-02-24 19:49:03 +0000

    Added editing product functionality

--- products/views.py
+++ products/views.py
@@ -84,11 +84,35 @@ def add_product(request):
             messages.error(request, 'Failed to add product!')
     else:
         form = ProductForm()
-        
-    form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request,template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit prodct in store """
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product.')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product!')
+    else:    
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request,template, context)
