3c37e69af0924d6b7bcf577ce86f210aee5fb14a
Karosko dev <t.karavasilev@gmail.com>
2024-02-24 14:17:50 +0000

    Add product view,url,template

--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Product, Category
 
 
-class ProductFarm(forms.ModelForm):
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
