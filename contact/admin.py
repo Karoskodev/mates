3fb576d9326d15425e6a110e74dd1b16cd30dadd
Karosko dev <t.karavasilev@gmail.com>
2024-02-26 20:50:28 +0000

    Update Contact admin fields display

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import contact
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(contact)
+
+
+@admin.register(contact)
+class ContactAdmin(SummernoteModelAdmin):
+
+    list_display = ('created_at', 'email', 'subject', 'message')
