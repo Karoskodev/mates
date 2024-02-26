56395cf858028bf5223cacddefa6c7054ac82cb7
Karosko dev <t.karavasilev@gmail.com>
2024-02-26 20:36:49 +0000

    created contact form view and template

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.Contact.as_view(), name="contact"),
+    path("", views.contact_form, name="contact_form"),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from .forms import ContactForm
+from django.contrib import messages
 
-# Create your views here.
+def contact_form(request):
+    """ Contact form view """
+
+    template = 'contact/contact.html'
+    form = ContactForm()
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.user = request.user
+            contact.save()
+            messages.success(request, "Form submited.")
+            return redirect(reverse("contact_form"))
+        else:
+            messages.error(request, "Failed to submit the form!")
+    
+    context = {
+        "form": form,
+    }
+    return render(request, template, context)
