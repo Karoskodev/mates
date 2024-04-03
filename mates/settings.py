82cb67e37fd7b5431a298afb5615a28af5d0ee3e
Karosko dev <t.karavasilev@gmail.com>
2024-04-03 08:53:32 +0000

    DEBUG set to true

--- mates/settings.py
+++ mates/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-karoskodev-mates-p2iz24bdqut.ws-eu108.gitpod.io', 'matesapp.herokuapp.com', 'localhost', 'matesapp-6452160599de.herokuapp.com']
 
