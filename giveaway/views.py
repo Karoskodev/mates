25a827c7b14483b80f8b5f1b63f49da05b35e661
Karosko dev <t.karavasilev@gmail.com>
2024-04-13 15:29:02 +0000

    Created giveaway form view

--- giveaway/views.py
+++ giveaway/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from .forms import GiveawayForm
+from django.contrib import messages
 
-# Create your views here.
+
+def giveaway_form(request):
+    """ giveaway form view """
+
+    template = 'giveaway/giveaway.html'
+    form = GiveawayForm()
+
+    if request.method == 'POST':
+        form = GiveawayForm(request.POST)
+        if form.is_valid():
+            giveaway = form.save(commit=False)
+            giveaway.user = request.user
+            giveaway.save()
+            messages.success(request, "Answer submited.")
+            return redirect(reverse("giveaway_form"))
+        else:
+            messages.error(request, "Failed to submit the answer!")
+
+    context = {
+        "form": form,
+    }
+    return render(request, template, context)
